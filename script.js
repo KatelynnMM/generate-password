6a5e3007208fb778a595327afbe74f3ef7232f9d
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2023-12-08 12:21:07 -0500

    add/button/css/js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var button = document.getElementById("myButton");
 
 // Write password to the #password input
 function writePassword() {
