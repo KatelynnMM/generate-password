812d00c2e93d62e450460857f175e0942557a2d6
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2023-12-08 19:16:49 -0500

    remove/ change direction

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var button = document.getElementById("myButton");
+
+function generatePassword() {
+  var length = prompt("Enter the lenght of the password");
+  var includeUppercase = confirm("Include uppercase letters?");
+  var includeLowercase = confirm("Include lowercase letters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special charcters?");
+
+  // password lengh 8<128
+
+  // lower, upper, number, special
+
+  // validate input
+
+  // generate based on criteria
+
+
+
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
