dc55678bc6841242371f311365b490dcb9d07088
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2023-12-09 16:29:40 -0500

    add arrays/length criteria

--- script.js
+++ script.js
@@ -8,17 +8,29 @@ function generatePassword() {
   var includeNumeric = confirm("Include numeric characters?");
   var includeSpecial = confirm("Include special charcters?");
 
-  // password lengh 8<128
-
-  // lower, upper, number, special
-
-  // validate input
-
-  // generate based on criteria
-
-
+  while (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid number between 8 and 128 for the password length.");
+    length = prompt("Enter the length of the password");
+  }
 
 }
+var uppercaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+
+
+
+// lower, upper, number, special
+
+// validate input
+
+// generate based on criteria
+
+
+
+
 
 
 
